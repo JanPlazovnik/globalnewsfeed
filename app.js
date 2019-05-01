2b1613d74cd5cd492b9347836829b411562f4c95
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-05-01 23:37:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use(express.static('node_modules'));
+app.use(express.static('assets'));
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
