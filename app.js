85b3ae87bb5c17642374eb9674548750ad04154e
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-05-01 23:36:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
