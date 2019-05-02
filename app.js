9ce8cbc8fea69fe17912cf0d33e6bf121c598756
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-05-02 02:12:13 +0200

    Update app.js
    i was lazy and i just copy pasted from the express hello world app don't mind me

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
