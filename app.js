2fde3e691298182e8d2df8fba7703519b97e85f3
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-05-03 16:06:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(process.env.PWD + '/assets'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
